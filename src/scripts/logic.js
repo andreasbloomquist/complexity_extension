f6ee248669d789da20a9a1e89993c9419cca4384
andreasbloomquist <andreasbloomquist@gmail.com>
2016-01-28 23:17:57 -0800

    beginning stage

---
+++ src/scripts/logic.js
@@ -0,0 +1,16 @@
+/*
+  This file contains logic for parsing the data object received from BuiltWith.
+  The purpose of this is to parse the object for complex technologies.
+  If a url is found to have a highly complex site (particularly front-end wise), then certain responses are set.
+  Below are the scores for how complex a site is that can be consumed by other functions.
+  
+  Scores:
+  1 = Not very complex
+  2 = Moderately complex
+  3 = Highly complex, call your SE to strategize
+*/
+
+console.log('loaded!!!!!!');
+
+function parseData(data){
+}
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Function to get the current tab from the Chrome api
 function getCurrentTabUrl(callback) {
   
   var queryInfo = {
@@ -13,13 +14,13 @@ function getCurrentTabUrl(callback) {
     console.assert(typeof url == 'string', 'tab.url should be a string');
     callback(url);
   });
-
 }
 
+// Function to get the data from the form input
 function getFormData(callback){
   console.log('getting data from the form...')
+
   var urlFormStr = document.getElementById('url-input').value;
-  
   console.log('String from input ', urlFormStr);
   
   if(urlFormStr != ''){
@@ -28,14 +29,16 @@ function getFormData(callback){
   }
 }
 
+// Function to call BW api via AJAX
 function getData(url, callback, errorCallback) {
   
   // Temporarily commenting out key until later
-  var key = '';
+  var key = 'b7012e96-e66d-47af-99d5-00545760acfe';
 
   //Using BuiltWith API
   var searchUrl = 'http://api.builtwith.com/v7/api.json?KEY=' + key + '&LOOKUP=builtwith.com';
 
+  // Start AJAX request
   var x = new XMLHttpRequest();
   x.open('GET', searchUrl);
   
@@ -50,10 +53,15 @@ function getData(url, callback, errorCallback) {
   x.send();
 }
 
-function renderStatus(resultText) {
-  document.getElementById('result').textContent = resultText;
+// Function to add status text to view
+function renderStatus(resultObj) {
+  console.log(resultObj);
+  document.getElementById('company').textContent = resultObj.Results[0].Meta.CompanyName;
+  document.getElementById('url').textContent = resultObj.Results[0].Lookup;
 }
 
+
+// Event listener for DOM ready, cache html elements as variables as soon as ready
 document.addEventListener('DOMContentLoaded', function() {
   var currentPageLink = document.getElementById('curr-pg-lnk'),
       statusEl = document.getElementById('status'),
@@ -61,13 +69,14 @@ document.addEventListener('DOMContentLoaded', function() {
       analyzeSub = document.getElementById('analyze-btn'),
       analyzeInput = document.getElementById('url-input');
 
-
+  // Adds event listern for clicks on current page link
   currentPageLink.addEventListener('click', function(e){
     e.preventDefault();
-
+    // fires function to get the data from the current tab and then from BW
     getCurrentTabUrl(function(url) {
       getData(url, function(response) {
-        renderStatus(JSON.stringify(response.Results[0]));
+        parseData(response);
+        renderStatus(response);
       }, function(errorMessage) {
         renderStatus('Sorry! :( ' + errorMessage);
       });
@@ -76,16 +85,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   analyzeSub.addEventListener('click', function(e){
     e.preventDefault();
-
+    // fires function to get the data from the input and then BW
     getFormData(function(url) {
       getData(url, function(response) {
-        renderStatus(JSON.stringify(response.Results[0]));
+        renderStatus(response); 
       }, function(errorMessage) {
         renderStatus('Sorry! :( ' + errorMessage);
       });
     });
   });
-
-
-
 });
