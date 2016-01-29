ff27d56bcc2d3582d99befc8001bfff0848c00f5
andreasbloomquist <andreasbloomquist@gmail.com>
2016-01-28 21:59:58 -0800

    stuffz

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -17,7 +17,7 @@ function getCurrentTabUrl(callback) {
 }
 
 function getFormData(callback){
-  console.log('getting data from the form...');
+  console.log('getting data from the form...')
   var urlFormStr = document.getElementById('url-input').value;
   
   console.log('String from input ', urlFormStr);
@@ -28,7 +28,6 @@ function getFormData(callback){
   }
 }
 
-
 function getData(url, callback, errorCallback) {
   
   // Temporarily commenting out key until later
@@ -37,45 +36,32 @@ function getData(url, callback, errorCallback) {
   //Using BuiltWith API
   var searchUrl = 'http://api.builtwith.com/v7/api.json?KEY=' + key + '&LOOKUP=builtwith.com';
 
-  //Starting ajax request
   var x = new XMLHttpRequest();
   x.open('GET', searchUrl);
   
   x.responseType = 'json';
-
-  //if the request loads ok
   x.onload = function() {
     var response = x.response;
     callback(response);
   };
-  //otherwise if the request errors
   x.onerror = function() {
     errorCallback('Network error.');
   };
   x.send();
 }
 
-//Function to add the text from the api call to the view
 function renderStatus(resultText) {
   document.getElementById('result').textContent = resultText;
 }
 
-//Extracted logic into renderCallBack function
-//This function takes a string and searches the BW api for that url
-//The result of that api call is rendered by invoking the renderStatus() function
 function renderCallback(url){
   getData(url, function(response) {
     renderStatus(JSON.stringify(response.Results[0]));
     }, function(errorMessage) {
       renderStatus('Sorry! :( ' + errorMessage);
-    });
-  });
+    }
+  )
 }
-
-//////////////////////
-//EVENT LISTENERS
-/////////////////////
-// Creating an event listener on the document for DOM READy
 
 document.addEventListener('DOMContentLoaded', function() {
   var currentPageLink = document.getElementById('curr-pg-lnk'),
@@ -84,20 +70,17 @@ document.addEventListener('DOMContentLoaded', function() {
       analyzeSub = document.getElementById('analyze-btn'),
       analyzeInput = document.getElementById('url-input');
 
-  //inside of DOM ready I'm adding a click listener on the current page link
-  // On firing of click action, the render callback is fired with the proper data
-  // This calls the api and renders the data accordingly
+
   currentPageLink.addEventListener('click', function(e){
     e.preventDefault();
     getCurrentTabUrl(renderCallback(url));
   });
-  
-  //Similar click listener for the form submission button
-  //After the button is clicked I pull the value from the input and then call the api
-  //and render the data accordingly
+
   analyzeSub.addEventListener('click', function(e){
     e.preventDefault();
     getFormData(renderCallback(url));
   });
+
+
 
 });
